0011b7602491326a13262d4b15ab186bec3aef39
Your Name <you@example.com>
2022-03-04 20:41:27 +0900

    Added view and template fpr detailed blg post as well as CSS for the site

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from turtle import title
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(title__contains = 'W')
     return render(request, 'blog/post_list.html',{'posts': posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request, 'blog/post_detail.html',{'post': post})
